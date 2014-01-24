23bd52af7acdc25d1cc731bcf9b5339fea034cad
robinharms <robin@betahaus.net>
2014-01-24 10:18:39 +0100

    Facebook image plugin

--- voteit/velruse/providers/facebook.py
+++ voteit/velruse/providers/facebook.py
@@ -1,14 +1,44 @@
 from betahaus.viewcomponent import view_action
 from pyramid.renderers import render
 from velruse import login_url
+from zope.interface import implementer
+from zope.component import adapter
+
+from voteit.core.models.interfaces import IProfileImage
+from voteit.core.models.interfaces import IUser
 
 from voteit.velruse import VoteITVelruseTSF as _
-
 from voteit.velruse.models import BaseOAuth2Plugin
 
 
 class FacebookAuth(BaseOAuth2Plugin):
     name = 'facebook'
+
+
+@implementer(IProfileImage)
+@adapter(IUser)
+class FacebookProfileImagePlugin(object):
+    name = u'facebook_profile_image'
+    title = _('Facebook')
+    description = _(u'facebook_profile_image_description',
+                    default=u"Your profile image is the same as the one you use on Facebook.")
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def url(self, size):
+        if not 'facebook' in self.context.auth_domains:
+            return None
+        oauth_userid = self.context.auth_domains['facebook'].get('oauth_userid')
+        if oauth_userid:
+            return u'http://graph.facebook.com/%(UID)s/picture' % {'UID': oauth_userid}
+    
+    def is_valid_for_user(self):
+        if 'facebook' in self.context.auth_domains:
+            oauth_userid = self.context.auth_domains['facebook']['oauth_userid']
+            if oauth_userid:
+                return True
+        return False
 
 
 @view_action('login_forms', 'facebook', title = _(u"Facebook"))
@@ -29,4 +59,5 @@ def connect_va(context, request, va, **kw):
 
 def includeme(config):
     config.registry.registerAdapter(FacebookAuth, name = FacebookAuth.name)
+    config.registry.registerAdapter(FacebookProfileImagePlugin, name = FacebookProfileImagePlugin.name)
     config.scan()
